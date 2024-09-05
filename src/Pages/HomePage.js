70b349801b6bf5c142d7fa11e6137d22702e0369
riswanank <riswana56@gmail.com>
2024-09-05 13:58:28 +0530

    first commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,17 @@
+import Specials from "../Components/Specials";
+
+function HomePage()
+{
+    return(
+        <>
+        <h1>
+           
+        </h1>
+        <div>
+      <Specials/>
+        </div>
+        </>
+
+    );
+}
+export default HomePage;
---
+++ src/Pages/Reservation.js
@@ -0,0 +1,13 @@
+import BookingPage from "../Components/BookingPage";
+
+function Reservation()
+{
+    return(
+
+        <>
+        <h1>Reserve a Table</h1>   
+        <BookingPage/>
+        </>
+    );
+}
+export default Reservation;
